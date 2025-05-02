bf721b2be48de987bb78e68443ddd3d1c8118c76
khandelwalpiyush19 <piyushkhandelwal482@gmail.com>
2025-05-02 18:10:41 +0530

    commit

---
+++ backend/app.js
@@ -0,0 +1,45 @@
+import express from "express";
+import connectDB from "./db/databse.js";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import userRouter from "./routes/userRoutes.js";
+import blogRouter from "./routes/blogRoutes.js";
+
+const app  = express();
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000
+
+connectDB();
+
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true
+}));
+
+app.use("/api/v1/user",userRouter);
+app.use("/api/v1/blog",blogRouter);
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+}); 
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+     res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    });
+
+})
